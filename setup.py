77792523b0e9290c939730391682b4914595390e
Jack Zhang <zhangmengyu10@gmail.com>
2021-01-28 21:35:50 -0800

    Change matplotlib required version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = {
         "dataclasses; python_version < '3.7'",
         "humanfriendly",
         "scipy>=1.4.1",
-        "matplotlib==3.1.0",
+        "matplotlib>=3.1.0",
         "pillow>=6.1.0",
         "editdistance==0.5.2",
         "ctc-segmentation>=1.4.0",
